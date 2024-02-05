c4dbc5242dce11844c52379453cd30faa2ab4e18
OWO Devs <developers.owo@gmail.com>
2024-02-05 08:25:30 +0100

    test(CON-260): create test project

---
+++ OWOGTAVTESTS/OWOGTAVTESTS.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OWOGTAVTESTS
+{
+	TEST_CLASS(OWOGTAVTESTS)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
