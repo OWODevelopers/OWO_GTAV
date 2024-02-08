f4933082a773a5cf1e03df53497ad0e89381565e
OWO Devs <developers.owo@gmail.com>
2024-02-08 08:25:55 +0100

    test(CON-294): create tests class for driving sensations

---
+++ OWOGTAVTESTS/FeelDrivingTests.cpp
@@ -0,0 +1,20 @@
+#include "CppUnitTest.h"
+#include "HapticDevice.h"
+#include "../OWOAPI/Domain/SensationsFactory.h"
+#include "MockBody.h"
+#include "../FeelDamage.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OWOGTAVTESTS
+{
+	TEST_CLASS(FeelDrivingTests)
+	{
+	public:
+
+		TEST_METHOD(FeelDamage_WhenHealthIs_BelowPreviousHealth)
+		{
+
+		}
+	};
+}
